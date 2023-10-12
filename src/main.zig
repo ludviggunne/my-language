523e31ac9e449adb26f1e69b8d3a829d7736364f
Ludvig Gunne Lindström <ludvig.gunne.lindstrom@proton.me>
2023-10-12 20:15:21 +0200

    memory managment and return codes

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,5 @@
 
-const std       = @import("std");
-
+const std         = @import("std");
 const Lexer       = @import("Lexer.zig");
 const Parser      = @import("Parser.zig");
 const SourceRef   = @import("SourceRef.zig");
@@ -8,23 +7,26 @@ const errors      = @import("errors.zig");
 const CodeGen     = @import("CodeGen.zig");
 const SymbolTable = @import("SymbolTable.zig");
 
-pub fn main() !void {
+pub fn main() !u8 {
 
     const stdout = std.io.getStdOut().writer();
 
-    // Read file
-    var alloc = std.heap.GeneralPurposeAllocator(.{}) {};
-    //defer _ = alloc.deinit();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}) {};
+    defer _ = gpa.deinit();
+    var alloc = gpa.allocator();
 
+    // Read file
     const cwd = std.fs.cwd();
     const file = try cwd.openFile("./program.l", .{});
-    const source = try file.readToEndAlloc(alloc.allocator(), 1024);
+    const source = try file.readToEndAlloc(alloc, 1024);
+    defer alloc.free(source);
+    file.close();
 
     // Lex file
     var lexer = Lexer.init(source);
 
     // Parse file
-    var parser = Parser.init(alloc.allocator(), &lexer);
+    var parser = Parser.init(alloc, &lexer);
     defer parser.deinit();
 
     var ast = parser.parse() catch {
@@ -33,13 +35,13 @@ pub fn main() !void {
             try errors.reportParseError(err, stdout, source);            
         }
 
-        return error.CompilationFailed;
+        return 1;
     };
 
     //@import("ast.zig").print(source, &ast);
 
     // Create symbol table
-    var symtab = try SymbolTable.init(alloc.allocator(), &ast, source);
+    var symtab = try SymbolTable.init(alloc, &ast, source);
     defer symtab.deinit();
 
     symtab.resolve() catch {
@@ -48,17 +50,20 @@ pub fn main() !void {
             try errors.reportSymbolResolutionError(err, stdout, source);
         }
 
-        return error.CompilationFailed;
+        return 1;
     };
 
     // Generate assembly
-    var codegen = CodeGen.init(&ast, alloc.allocator(), &symtab);
+    var codegen = CodeGen.init(&ast, alloc, &symtab);
     defer codegen.deinit();
 
     codegen.initWriters();
     try codegen.generate();
 
+    // Output
     var output = try cwd.createFile("./output.S", .{});
     var out_writer = output.writer();
     try codegen.output(&out_writer);
+
+    return 0;
 }
